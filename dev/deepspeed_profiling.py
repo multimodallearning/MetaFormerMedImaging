22ba5c8e4a1d4f21da0c10305a450a79078e5a4b
Ron Keuth <r.keuth@uni-luebeck.de>
2025-05-09 09:32:25 +0200

    fix deepspeed profiler

--- dev/deepspeed_profiling.py
+++ dev/deepspeed_profiling.py
@@ -1,7 +1,11 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
 import torch
 from deepspeed.profiling.flops_profiler import get_model_profile
 from torch import nn
-from architectures.flex_modules import FlexBlock
+from architectures.flex_token_mixer import FlexTokenBlock
+from architectures.poolformer import PoolFormerBlock
 import pandas as pd
 from tqdm import tqdm
 
@@ -15,8 +19,9 @@ def profile(model: torch.nn.Module, input_shape: tuple[int, ...]):
 results_df = pd.DataFrame(columns=['operation', 'kernel', 'spatial', 'flops', 'macs', 'params'])
 done_4_full_atn = False
 for kernel_size in tqdm([3, 5, 7, 9], desc='Kernel'):
-    for config in [(64, 128), (128, 64), (256, 32), (512, 16)]:
+    for config in [(64, 128), (128, 64), (256, 32), (512, 16), (1024, 8), (2048, 4)]:
         n_channel, spatial_dim = config
+        n_channel = 64
         kernel_label = f'{kernel_size}²'
         spatial_label = f'{n_channel}x{spatial_dim}²'
 
@@ -24,9 +29,17 @@ for kernel_size in tqdm([3, 5, 7, 9], desc='Kernel'):
         flops, macs, params = profile(conv, (1, n_channel, spatial_dim, spatial_dim))
         results_df.loc[len(results_df.index)] = ['conv', kernel_label, spatial_label, flops, macs, params]
 
-        flex = FlexBlock(torch.tensor([spatial_dim, spatial_dim]), kernel_size, n_channel, 4, 1, 4, 'cuda').cuda()
-        flops, macs, params = profile(flex, (1, spatial_dim ** 2, n_channel))
-        results_df.loc[len(results_df.index)] = ['flex', kernel_label, spatial_label, flops, macs, params]
+        pool = PoolFormerBlock(n_channel, kernel_size).cuda()
+        flops, macs, params = profile(pool, (1, n_channel, spatial_dim, spatial_dim))
+        results_df.loc[len(results_df.index)] = ['pool', kernel_label, spatial_label, flops, macs, params]
+
+        flex = FlexTokenBlock(4, [spatial_dim, spatial_dim], 'cuda', n_channel, kernel_size).cuda()
+        try:
+            flops, macs, params = profile(flex, (1, n_channel, spatial_dim, spatial_dim))
+            results_df.loc[len(results_df.index)] = ['flex', kernel_label, spatial_label, flops, macs, params]
+        except:
+            results_df.loc[len(results_df.index)] = ['flex', kernel_label, spatial_label, 'N/A', 'N/A', 'N/A']
+
 
         if not done_4_full_atn:
             full_attn = nn.TransformerEncoderLayer(n_channel, 4, n_channel * 4, norm_first=True, batch_first=True, activation=torch.nn.functional.leaky_relu).cuda()
--- dev/plot_profiling_results.py
+++ dev/plot_profiling_results.py
@@ -5,25 +5,31 @@ from matplotlib import pyplot as plt
 df = pd.read_csv('dev/results.csv')
 for spatial_dim, g_df in df.groupby('spatial'):
     row_full_atn = g_df[g_df['operation'] == 'full_attn']
-    g_df = g_df[g_df['operation'] != 'full_attn']
+    # g_df = g_df[g_df['operation'] != 'full_attn']
+    g_df = g_df[g_df['operation'] == 'flex']
     full_attn_hline_kwargs = dict(xmin=0, xmax=len(g_df['kernel'].unique()) - 1, colors='r', label='full_attn',
                                   linestyle='--')
 
     fig, axs = plt.subplots(1, 3)
     fig.suptitle(spatial_dim)
 
-    sns.lineplot(x='kernel', y='flops', hue='operation', data=g_df, ax=axs[0])
-    axs[0].hlines(row_full_atn['flops'].values, **full_attn_hline_kwargs)
+    ln_plot_kwargs = dict(x='kernel', hue='operation', data=g_df)
+
+    sns.lineplot(y='flops', ax=axs[0], **ln_plot_kwargs)
+    # axs[0].hlines(row_full_atn['flops'].values, **full_attn_hline_kwargs)
+    # axs[0].set_yscale('log')
     axs[0].legend()
     axs[0].set_title('FLOPS')
 
-    sns.lineplot(x='kernel', y='macs', hue='operation', data=g_df, ax=axs[1])
-    axs[1].hlines(row_full_atn['macs'].values, **full_attn_hline_kwargs)
+    sns.lineplot(y='macs', ax=axs[1], **ln_plot_kwargs)
+    # axs[1].hlines(row_full_atn['macs'].values, **full_attn_hline_kwargs)
+    # axs[1].set_yscale('log')
     axs[1].legend()
     axs[1].set_title('MACs')
 
-    sns.lineplot(x='kernel', y='params', hue='operation', data=g_df, ax=axs[2])
-    axs[2].hlines(row_full_atn['params'].values, **full_attn_hline_kwargs)
+    sns.lineplot(y='params', ax=axs[2], **ln_plot_kwargs)
+    # axs[2].hlines(row_full_atn['params'].values, **full_attn_hline_kwargs)
+    # axs[2].set_yscale('log')
     axs[2].legend()
     axs[2].set_title('#Params')
 
