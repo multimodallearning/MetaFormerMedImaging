b38e5388c8a4b24ba4bc401ba9988826a4508e64
Ron Keuth <r.keuth@uni-luebeck.de>
2025-05-27 10:46:50 +0200

    add manual unravel index for 3d

--- architectures/test_utils.py
+++ architectures/test_utils.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 import torch
-from utils import unravel_index
+from utils import unravel_index, unravel_index_2d, unravel_index_3d
 from tqdm import trange
 
 
-class Test(TestCase):
+class TestGeneralUnravel(TestCase):
     def test_2d(self):
         shape = [4, 2]
         for idx in trange(shape[0] * shape[1]):
@@ -20,3 +20,21 @@ class Test(TestCase):
             expected = [e.item() for e in expected]
             actual = unravel_index(idx, shape)
             self.assertEqual(expected, actual)
+
+
+class TestSpecificUnravel(TestCase):
+    def test_2d(self):
+        shape = [4, 2]
+        for idx in trange(shape[0] * shape[1]):
+            expected = torch.unravel_index(torch.tensor(idx), shape)
+            expected = [e.item() for e in expected]
+            actual = unravel_index_2d(idx, shape)
+            self.assertEqual(expected, list(actual))
+
+    def test_3d(self):
+        shape = [64, 32, 16]
+        for idx in trange(shape[0] * shape[1] * shape[2]):
+            expected = torch.unravel_index(torch.tensor(idx), shape)
+            expected = [e.item() for e in expected]
+            actual = unravel_index_3d(idx, shape)
+            self.assertEqual(expected, list(actual))
--- architectures/utils.py
+++ architectures/utils.py
@@ -10,7 +10,14 @@ def unravel_index(idx: int, shape: List[int]) -> List[int]:
     return unraveled[::-1]
 
 def unravel_index_2d(idx: int, shape: List[int]) -> tuple[int, int]:
-    x = idx % shape[1]
-    y = idx // shape[1]
+    w = idx % shape[1]
+    h = idx // shape[1]
 
-    return x, y
+    return h, w
+
+def unravel_index_3d(idx: int, shape: List[int]) -> tuple[int, int, int]:
+    d = idx % shape[2]
+    w = (idx // shape[2]) % shape[1]
+    h = idx // (shape[2] * shape[1])
+
+    return h, w, d
