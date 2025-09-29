fix wrong statistic test and adapt ranking segmentation average

eval/rank_segmentation.py
@@ -8,34 +8,27 @@ from eval import rank_utils
 # construct metric matrix
 dsc_dir = Path('eval/dsc_scores')
 ds_dirs = sorted(list(dsc_dir.glob('*')))
-tm_names = list()
 df = dict()
 df_normalize = dict()
 for ds in ds_dirs:
     dsc_values = list()
-    temp_tm_names = list()
+    tm_names = list()
     for tm in sorted(list(ds.glob('*.pth'))):
         dsc_values.append(torch.load(str(tm), weights_only=True))
-        temp_tm_names.append(tm.stem)
+        tm_names.append(tm.stem)
     dsc_values = torch.stack(dsc_values)
-    if len(tm_names) > 0:
-        assert temp_tm_names == tm_names
-    else:  # first iteration
-        tm_names = temp_tm_names
-
     # ranking
-    ranking_scores = rank_utils.scores_better(dsc_values, 'ranksums')
-    df[ds.name] = ranking_scores.tolist()
+    ranking_scores = rank_utils.scores_better(dsc_values, 'signed-rank')
+    df[ds.name] = pd.Series(index=tm_names,  data=ranking_scores.tolist())
     ranking_scores_normalized = rank_utils.rankscore_avgtie(ranking_scores)
-    df_normalize[ds.name] = ranking_scores_normalized.tolist()
+    df_normalize[ds.name] = pd.Series(index=tm_names,  data=ranking_scores_normalized.tolist())
 
-df = pd.DataFrame.from_dict(df)
-df.insert(0, 'TokenMixer', tm_names)
-df.set_index('TokenMixer', inplace=True)
+df = pd.DataFrame(df)
 print(df.to_string())
 
-df_normalize = pd.DataFrame.from_dict(df_normalize)
-df_normalize.insert(0, 'TokenMixer', tm_names)
-df_normalize.set_index('TokenMixer', inplace=True)
-df_normalize['mean'] = df_normalize.mean(1)
+df_normalize = pd.DataFrame(df_normalize)
+df_normalize['mean'] = df_normalize[~df_normalize.index.str.endswith('_9')].mean(1, skipna=True)
+df_normalize.sort_values(by='mean', ascending=False, inplace=True)
 print(df_normalize.to_string())
+
+print(df_normalize.sort_values(by='TIGERDataModule', ascending=False).to_string())

eval/rank_utils.py
@@ -2,7 +2,7 @@
 import itertools
 
 import torch
-from scipy.stats import ranksums
+from scipy.stats import ranksums, wilcoxon
 
 
 def scores_better(tasks_metric: torch.Tensor, test_method:str, alpha: float = 0.05):
@@ -16,13 +16,17 @@ def scores_better(tasks_metric: torch.Tensor, test_method:str, alpha: float = 0.
     T, S = tasks_metric.shape
     better = torch.full((T, T), -1)
     for t_curr, t_comp in itertools.product(range(T), repeat=2):
-        if test_method == "ranksums":
+        if t_curr == t_comp: # skip test to itself
+            better[t_curr, t_comp] = 0
+            continue
+
+        if test_method == "ranksums": # when samples are independent by each other (e.g. different datasets)
             h, p = ranksums(tasks_metric[t_curr].numpy(), tasks_metric[t_comp].numpy())
             if h > 0 and p < alpha:  # sign of h and p-value
                 better[t_curr, t_comp] = 1
             else:
                 better[t_curr, t_comp] = 0
-        elif test_method == "CI-based":
+        elif test_method == "CI-based": # paired test when using bootstrapping (5000 repeats are recommended)
             diffs = tasks_metric[t_curr] - tasks_metric[t_comp]
             ci_low = torch.quantile(diffs, alpha/2)
             ci_high = torch.quantile(diffs, 1-alpha/2)
@@ -30,8 +34,15 @@ def scores_better(tasks_metric: torch.Tensor, test_method:str, alpha: float = 0.
                 better[t_curr, t_comp] = 1 if diffs.median() > 0 else 0 # and better
             else:
                 better[t_curr, t_comp] = 0  # non significant
+        elif test_method == "signed-rank": # paired test for small sample sizes
+            diffs = tasks_metric[t_curr] - tasks_metric[t_comp]
+            _, p = wilcoxon(diffs.numpy())
+            if diffs.median() > 0 and p < alpha:
+                better[t_curr, t_comp] = 1
+            else:
+                better[t_curr, t_comp] = 0
         else:
-            raise ValueError(f"Unknown test_method: {test_method}. Please select from [ranksums, CI-based]")
+            raise ValueError(f"Unknown test_method: {test_method}. Please select from [ranksums, CI-based, signed-rank]")
     assert (better != -1).all(), 'some comparisons have not been made'
     scores_task = better.sum(1)
     return scores_task